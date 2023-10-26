3e3a602c395bd30e5526b07b7db04f45527790b9
Lean Obregon <lean.obregon@fiserv.com>
2023-10-26 16:42:33 -0400

    updated

--- src/Abstraction/Car.java
+++ src/Abstraction/Car.java
@@ -1,5 +1,6 @@
 package Abstraction;
 
 public class Car extends Vehicle{
-    
+    public Car(int totalMilage, double price, String make, String model) {
+    }
 }
--- src/Encapsulation/Employee.java
+++ src/Encapsulation/Employee.java
@@ -1,5 +1,13 @@
 package Encapsulation;
 
 public class Employee extends Person {
-    
+    int salary;
+
+    public Employee(int salary) {
+        this.salary = salary;
+    }
+
+    public String speak() {
+        return "My name is " + firstName + " " + lastName + " and my salary is $" + salary;
+    }
 }
